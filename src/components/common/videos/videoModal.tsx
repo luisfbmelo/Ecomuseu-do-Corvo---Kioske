2526711f7fcf4aaa569586f272e71fc35be0c2de
Luis Melo <luisfbmelo91@gmail.com>
2019-11-26 10:15:59 -0100

    Updated path of modal video styles

--- src/components/common/videos/videoModal.tsx
+++ src/components/common/videos/videoModal.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ModalVideoStyles from 'assets/theme//components/videos/modalVideo';
+import ModalVideoStyles from 'assets/theme/components/videos/modalVideo';
 
 export default (props: any) => {
   if(!props.isOpen){
