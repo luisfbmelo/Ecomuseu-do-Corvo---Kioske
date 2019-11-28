ce47535599c0d34c96bd94b88d62d11120d5334e
Luis Melo <luisfbmelo91@gmail.com>
2019-11-28 15:05:44 -0100

    Fixed issue with updater translation

--- src/components/common/updater.tsx
+++ src/components/common/updater.tsx
@@ -95,7 +95,7 @@ export default class Updater extends Component<IProps, IState>{
       return (
         <Fragment>
           <p><Trans>A aplicação possui uma atualização.</Trans></p>
-          <p><Trans>A descarregar: {this.state.progress}%</Trans></p>
+          <p><Trans>A descarregar:</Trans> {this.state.progress}%</p>
         </Fragment>
       )
     }
