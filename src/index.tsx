d6011e3a1ce2aa1453ad81eb73646e855b57a348
Luis Melo <luisfbmelo91@gmail.com>
2020-03-17 15:07:09 -0100

    Removed unecessary component

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import Root from './Root';
 import CustomRoutes from './routes';
 import * as serviceWorker from './serviceWorker';
-
-import { I18nextProvider } from "react-i18next";
 import "translations/i18n";
 
 ReactDOM.render(
